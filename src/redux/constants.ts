9220468a4869c9e089efaf51a4d633a7fdea4a39
Tony Mamo <tony.mamo@galileo-insights.com>
2020-02-26 20:13:41 -0500

    use GATSBY_API_LOCATION as env var

--- src/redux/constants.ts
+++ src/redux/constants.ts
@@ -1,4 +1,4 @@
-export const BASE_ROOT = process.env.REACT_APP_API_LOCATION;
+export const BASE_ROOT = process.env.GATSBY_API_LOCATION;
 export const API_VERSION = 'v1';
 export const API_ROOT = BASE_ROOT ? `${BASE_ROOT}/api/${API_VERSION}` : '';
 
