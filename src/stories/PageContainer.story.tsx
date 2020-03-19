58ff3d3231c54fc759517f162eba06a3d92a0dc5
michelleyj <michelleduhaime91@gmail.com>
2020-03-19 14:49:59 -0400

    created storybook file for PageContainer component

---
+++ src/stories/PageContainer.story.tsx
@@ -0,0 +1,13 @@
+import React, { FunctionComponent } from 'react';
+import { storiesOf } from '@storybook/react';
+
+import PageContainer from '../components/PageContainer';
+
+const story = storiesOf('Components', module);
+
+story.add(
+  'PageContainer',
+  () => (
+    <PageContainer>Hello World!</PageContainer>
+  ),
+);
