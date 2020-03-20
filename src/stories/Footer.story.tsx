b27f723b5bbb875b616185127b269d9b610c62d0
Kyle Trusler <kyletrusler@gmail.com>
2020-03-20 14:06:28 -0400

    add auto to gitattributes

--- src/stories/Footer.story.tsx
+++ src/stories/Footer.story.tsx
@@ -5,10 +5,4 @@ import Footer from '../components/Footer';
 
 const story = storiesOf('Components', module);
 
-
-story.add(
-  'Footer',
-  () => (
-    <Footer />
-  ),
-);
+story.add('Footer', () => <Footer />);
