3005562d6f1396b0267ddfd8c3688a3d3dee55fd
marmielemon <50599564+marmielemon@users.noreply.github.com>
2020-03-20 10:32:04 -0400

    Adding a story file for footer

---
+++ src/stories/Footer.story.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+
+import Footer from '../components/Footer';
+
+const story = storiesOf('Components', module);
+
+
+story.add(
+  'Footer',
+  () => (
+    <Footer />
+  ),
+);
