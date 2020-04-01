77fe8e87ce4f975d8e794e2bcb87841395ab35ea
michelleyj <michelleduhaime91@gmail.com>
2020-04-01 18:13:14 -0400

    added Formik component to Security file

--- src/views/agent/Authenticated/Account/Security.tsx
+++ src/views/agent/Authenticated/Account/Security.tsx
@@ -1,10 +1,59 @@
 import React, { FunctionComponent } from 'react';
+import { Formik, Field, Form } from 'formik';
 import { RouteComponentProps } from '@reach/router';
 
-import { Heading } from '../../../../components';
+import { Heading, Box, Input, Button } from '../../../../components';
+
+import { requiredPassword, passwordRulesString } from '../../../../utils/validations';
 
 type AgentSecurityProps = {} & RouteComponentProps;
 
-const AgentSecurity: FunctionComponent<AgentSecurityProps> = () => <Heading>Security</Heading>;
+const AgentSecurity: FunctionComponent<AgentSecurityProps> = () => {
+  const initialValues = {
+    currentPassword: '',
+    newPassword: '',
+    confirmPassword: '',
+  };
+  return (
+    <>
+      <Box>
+        <Heading>Security</Heading>
+        <Formik
+          validateOnMount
+          initialValues={initialValues}
+          onSubmit={(values) => console.log(values)}
+        >
+          {() => (
+            <Form>
+              <Field
+                as={Input}
+                type="password"
+                name="currentPassword"
+                label="Current Password"
+              />
+              <Field
+                as={Input}
+                type="password"
+                name="newPassword"
+                label="New Password"
+                validate={requiredPassword}
+                helpText={passwordRulesString}
+              />
+              <Field
+                as={Input}
+                type="password"
+                name="confirmPassword"
+                label="Confirm Password"
+              />
+            </Form>
+          )}
+        </Formik>
+        <Button type="submit" color="text" block>
+          Submit
+        </Button>
+      </Box>
+    </>
+  );
+};
 
 export default AgentSecurity;
