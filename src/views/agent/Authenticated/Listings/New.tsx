eb3a4e423ed54e5485411e1c4ac9bf4d7dd7b90d
Tony Mamo <tony.mamo@galileo-insights.com>
2020-04-01 14:20:40 -0400

    empty view for New, Pending, Awarded, and History listings pages

--- src/views/agent/Authenticated/Listings/New.tsx
+++ src/views/agent/Authenticated/Listings/New.tsx
@@ -1,13 +1,37 @@
 import React, { FunctionComponent } from 'react';
 import { RouteComponentProps } from '@reach/router';
 import addHours from 'date-fns/addHours';
+import { FaFrown } from 'react-icons/fa';
 
-import { BidCard, Column, Heading, Seo, Row } from '../../../../components';
+import {
+  Box,
+  Button,
+  BidCard,
+  Column,
+  Heading,
+  Seo,
+  Row,
+  FlexContainer,
+} from '../../../../components';
+import { doubleSpacer } from '../../../../styles/size';
 
 const NewListings: FunctionComponent<RouteComponentProps> = () => (
   <>
     <Seo title="New Listings" />
     <Heading>New Listings</Heading>
+    {true && ( // TODO: look at new listings length to show this dynamically
+      <Box>
+        <FlexContainer height="500px" flexDirection="column">
+          <FaFrown fontSize={64} style={{ margin: doubleSpacer }} />
+          <Heading styledAs="title" align="center">
+            There are no new bids in your current sales area.
+          </Heading>
+          <Button type="link" to="/agent/account/settings">
+            Add More Cities
+          </Button>
+        </FlexContainer>
+      </Box>
+    )}
     <Row>
       <Column sm={6} lg={4}>
         <BidCard
@@ -23,54 +47,6 @@ const NewListings: FunctionComponent<RouteComponentProps> = () => (
           type="buying"
           location={['48170', '48152']}
           expiresAt={addHours(new Date(), 1)}
-        />
-      </Column>
-      <Column sm={6} lg={4}>
-        <BidCard
-          priceRange="$350-375k"
-          type="selling"
-          location="48178"
-          expiresAt={addHours(new Date(), 10)}
-        />
-      </Column>
-      <Column sm={6} lg={4}>
-        <BidCard
-          priceRange="$350-375k"
-          type="buying"
-          location={['48170', '48152']}
-          expiresAt={addHours(new Date(), 13)}
-        />
-      </Column>
-      <Column sm={6} lg={4}>
-        <BidCard
-          priceRange="$350-375k"
-          type="selling"
-          location="48178"
-          expiresAt={addHours(new Date(), 22)}
-        />
-      </Column>
-      <Column sm={6} lg={4}>
-        <BidCard
-          priceRange="$350-375k"
-          type="buying"
-          location={['48170', '48152']}
-          expiresAt={addHours(new Date(), 1)}
-        />
-      </Column>
-      <Column sm={6} lg={4}>
-        <BidCard
-          priceRange="$350-375k"
-          type="selling"
-          location="48178"
-          expiresAt={addHours(new Date(), 10)}
-        />
-      </Column>
-      <Column sm={6} lg={4}>
-        <BidCard
-          priceRange="$350-375k"
-          type="buying"
-          location={['48170', '48152']}
-          expiresAt={addHours(new Date(), 13)}
         />
       </Column>
     </Row>
