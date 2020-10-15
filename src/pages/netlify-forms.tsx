d321decd3de1b8916f96a72d434be2ac3b818787
Tony Mamo <tony.mamo@galileo-insights.com>
2020-10-15 15:07:25 -0400

    add hidden inputs for every form value in hidden forms

--- src/pages/netlify-forms.tsx
+++ src/pages/netlify-forms.tsx
@@ -18,22 +18,91 @@ const NetlifyForms: FunctionComponent<{}> = () => (
     </Card>
     <Formik initialValues={{}} onSubmit={() => console.log('hello')}>
       {() => (
-        <Form name="contact" method="post" netlify-honeypot="bot-field" data-netlify="true">
+        <Form
+          name="get-financed-form"
+          method="post"
+          netlify-honeypot="bot-field"
+          data-netlify="true"
+        >
           <input type="hidden" name="form-name" value="get-financed-form" />
+          {[
+            'firstName',
+            'lastName',
+            'email',
+            'phoneNumber',
+            'city',
+            'state',
+            'buyingPriceRange',
+            'preApproved',
+            'freeMortgageConsult',
+            'subject',
+          ].map((name) => (
+            <input type="hidden" name={name} value="" key={name} />
+          ))}
         </Form>
       )}
     </Formik>
     <Formik initialValues={{}} onSubmit={() => console.log('hello')}>
       {() => (
-        <Form name="contact" method="post" netlify-honeypot="bot-field" data-netlify="true">
+        <Form name="missing-city" method="post" netlify-honeypot="bot-field" data-netlify="true">
           <input type="hidden" name="form-name" value="missing-city" />
+          {[
+            'firstName',
+            'lastName',
+            'phoneNumber',
+            'email',
+            'state',
+            'zip',
+            'buyingCities',
+            'buyingPriceRange',
+            'sellersAddressLine1',
+            'sellersAddressLine2',
+            'sellersCity',
+            'sellersListingPriceInMind',
+            'subject',
+          ].map((name) => (
+            <input type="hidden" name={name} value="" key={name} />
+          ))}
         </Form>
       )}
     </Formik>
     <Formik initialValues={{}} onSubmit={() => console.log('hello')}>
       {() => (
-        <Form name="contact" method="post" netlify-honeypot="bot-field" data-netlify="true">
+        <Form
+          name="free-mortgage-consultation"
+          method="post"
+          netlify-honeypot="bot-field"
+          data-netlify="true"
+        >
           <input type="hidden" name="form-name" value="free-mortgage-consultation" />
+          {[
+            'type',
+            'buyingCities',
+            'buyingPriceRangeId',
+            'createDateTime',
+            'freeMortgageConsult',
+            'preApproved',
+            'sellersAddressLine1',
+            'sellersAddressLine2',
+            'sellersCity',
+            'sellersZip',
+            'sellersTimeline',
+            'sellersListingPriceInMindPriceRangeInMindId',
+            'sellersMortgageBalanceId',
+            'agentSubmittedBidId',
+            'buyerTypeOfHomeId',
+            'sellerTypeOfHomeId',
+            'city',
+            'state',
+            'buyingPriceRange',
+            'firstName',
+            'lastName',
+            'email',
+            'phoneNumber',
+            'subject',
+          ].map((name) => (
+            <input type="hidden" name={name} value="" key={name} />
+          ))}
         </Form>
       )}
     </Formik>
