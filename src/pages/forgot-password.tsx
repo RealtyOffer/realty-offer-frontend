a76fdd9e8cb5dfedcbc4ff87fa440f1d72ae732c
Kevin Shreve <kshreve@gmail.com>
2020-10-15 23:50:29 -0400

    [REAL-249] - forgot-password.tsx

--- src/pages/forgot-password.tsx
+++ src/pages/forgot-password.tsx
@@ -1,6 +1,6 @@
 import React, { FunctionComponent } from 'react';
 import { Formik, Field, Form } from 'formik';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { navigate } from 'gatsby';
 
 import { PageContainer, Button, Card, Input, HorizontalRule, Seo } from '../components';
@@ -9,12 +9,13 @@ import { requiredEmail } from '../utils/validations';
 import { ActionResponseType } from '../redux/constants';
 import { forgotPassword } from '../redux/ducks/auth';
 import { addAlert } from '../redux/ducks/globalAlerts';
+import { RootState } from '../redux/ducks';
 
 type ForgotPasswordProps = {};
 
 const ForgotPassword: FunctionComponent<ForgotPasswordProps> = () => {
   const dispatch = useDispatch();
-
+  const auth = useSelector((state: RootState) => state.auth);
   const initialValues = {
     email: '',
   };
@@ -53,8 +54,13 @@ const ForgotPassword: FunctionComponent<ForgotPasswordProps> = () => {
                 required
               />
               <HorizontalRule />
-              <Button type="submit" disabled={isSubmitting || !isValid} block>
-                Submit
+              <Button
+                type="submit"
+                disabled={isSubmitting || !isValid}
+                block
+                isLoading={isSubmitting || auth.isLoading}
+              >
+                {isSubmitting || auth.isLoading ? 'Submitting' : 'Submit'}
               </Button>
               <Button type="link" to="/login" color="text" block>
                 Cancel
