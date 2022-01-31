bd4899891a978a64584203d952684d02b28f8fdf
Tony Mamo <tmamo13991@gmail.com>
2022-01-31 13:50:36 -0500

    FAQ page tweaks

--- src/templates/frequently-asked-questions.tsx
+++ src/templates/frequently-asked-questions.tsx
@@ -1,8 +1,8 @@
-import React, { FunctionComponent } from 'react';
-import { Link, graphql } from 'gatsby';
+import React, { FunctionComponent, useState } from 'react';
+import { graphql } from 'gatsby';
 import { FixedObject, FluidObject } from 'gatsby-image';
 import ReactMarkdown from 'react-markdown/with-html';
-import styled from 'styled-components';
+
 import {
   Accordion,
   AccordionItem,
@@ -11,21 +11,12 @@ import {
   AccordionItemPanel,
   AccordionItemState,
 } from 'react-accessible-accordion';
-import { FaChevronCircleUp, FaChevronCircleDown } from 'react-icons/fa';
+import { FaChevronCircleUp, FaChevronCircleDown, FaFilter } from 'react-icons/fa';
 
-import {
-  Box,
-  Button,
-  FlexContainer,
-  HeroImage,
-  Heading,
-  PageContainer,
-  Seo,
-  HorizontalRule,
-} from '../components';
+import { Box, Button, FlexContainer, HeroImage, Heading, PageContainer, Seo } from '../components';
 
-import { doubleSpacer, baseSpacer, breakpoints } from '../styles/size';
-import { lightestGray } from '../styles/color';
+import { doubleSpacer, baseSpacer, tripleSpacer } from '../styles/size';
+import { brandPrimary, brandTertiary } from '../styles/color';
 
 type FAQPageProps = {
   title: string;
@@ -49,15 +40,6 @@ type FAQPageProps = {
   };
 };
 
-const HeroBox = styled.div`
-  background-color: rgba(0, 0, 0, 0.4);
-  padding: ${baseSpacer};
-  max-width: ${breakpoints.md};
-  @media only screen and (min-width: ${breakpoints.sm}) {
-    padding: ${doubleSpacer};
-  }
-`;
-
 export const FAQPageTemplate: FunctionComponent<FAQPageProps> = ({
   heroImage,
   heroHeading,
@@ -67,130 +49,155 @@ export const FAQPageTemplate: FunctionComponent<FAQPageProps> = ({
   metaTitle,
   metaDescription,
   metaKeywords,
-}) => (
-  <div>
-    <Seo
-      title={metaTitle}
-      description={metaDescription}
-      meta={[{ name: 'keywords', content: metaKeywords }]}
-      image={heroImage.childImageSharp.fixed.src}
-      imageWidth={heroImage.childImageSharp.fixed.width}
-      imageHeight={heroImage.childImageSharp.fixed.height}
-    />
-    <HeroImage imgSrc={heroImage}>
-      <HeroBox>
-        <Heading inverse>{heroHeading}</Heading>
-        <Heading inverse as="h2">
-          {heroSubheading}
-        </Heading>
-      </HeroBox>
-    </HeroImage>
-    <section style={{ padding: `${doubleSpacer} 0` }}>
-      <PageContainer>
-        <Box>
-          <p>
-            <Link to="/">Home</Link> &gt; FAQs
+}) => {
+  const [filter, setFilter] = useState<'all' | 'consumers' | 'agents'>('all');
+  return (
+    <div>
+      <Seo
+        title={metaTitle}
+        description={metaDescription}
+        meta={[{ name: 'keywords', content: metaKeywords }]}
+        image={heroImage.childImageSharp.fixed.src}
+        imageWidth={heroImage.childImageSharp.fixed.width}
+        imageHeight={heroImage.childImageSharp.fixed.height}
+      />
+      <HeroImage imgSrc={heroImage} />
+      <section style={{ padding: `${tripleSpacer} 0` }}>
+        <PageContainer>
+          <Heading styledAs="title">{heroHeading}</Heading>
+          <Heading as="h5">{heroSubheading}</Heading>
+
+          <p style={{ margin: `${doubleSpacer} 0` }}>
+            <FaFilter /> <span style={{ marginRight: baseSpacer }}>Filter By:</span>
+            <Button
+              type="button"
+              onClick={() => setFilter('all')}
+              rightspacer
+              color={filter === 'all' ? 'primary' : 'primaryOutline'}
+            >
+              All
+            </Button>
+            <Button
+              type="button"
+              onClick={() => setFilter('agents')}
+              color={filter === 'agents' ? 'primary' : 'primaryOutline'}
+              rightspacer
+            >
+              Agents
+            </Button>
+            <Button
+              type="button"
+              onClick={() => setFilter('consumers')}
+              rightspacer
+              color={filter === 'consumers' ? 'primary' : 'primaryOutline'}
+            >
+              Consumers
+            </Button>
           </p>
-          <Heading styledAs="title" as="h2">
-            Frequently Asked Questions (FAQs)
-          </Heading>
-          <br />
-          <Heading as="h3" styledAs="subtitle">
-            Consumers looking to buy or sell FAQs
-          </Heading>
-          <HorizontalRule />
+
           <Accordion allowZeroExpanded>
-            {consumerFaq.items.map((item) => (
-              <AccordionItem key={item.question}>
-                <AccordionItemHeading>
-                  <AccordionItemButton>
-                    <FlexContainer justifyContent="space-between" alignItems="center">
-                      <Heading as="h4" noMargin>
-                        {item.question}
-                      </Heading>
-                      <AccordionItemState>
-                        {({ expanded }) =>
-                          expanded ? (
-                            <FaChevronCircleUp color={lightestGray} size={doubleSpacer} />
-                          ) : (
-                            <FaChevronCircleDown color={lightestGray} size={doubleSpacer} />
-                          )
-                        }
-                      </AccordionItemState>
-                    </FlexContainer>
-                  </AccordionItemButton>
-                </AccordionItemHeading>
-                <AccordionItemPanel>
-                  <div style={{ paddingTop: baseSpacer }}>
-                    <ReactMarkdown source={item.answer} />
-                  </div>
-                </AccordionItemPanel>
-                <HorizontalRule />
-              </AccordionItem>
-            ))}
+            {filter !== 'agents' &&
+              consumerFaq.items.map((item) => (
+                <AccordionItem key={item.question}>
+                  <Box>
+                    <AccordionItemHeading>
+                      <AccordionItemButton>
+                        <FlexContainer
+                          justifyContent="space-between"
+                          alignItems="center"
+                          flexWrap="nowrap"
+                        >
+                          <Heading as="h4" noMargin>
+                            {item.question}
+                          </Heading>
+                          <AccordionItemState>
+                            {({ expanded }) =>
+                              expanded ? (
+                                <FaChevronCircleUp
+                                  color={brandPrimary}
+                                  size={doubleSpacer}
+                                  style={{ flexShrink: 0 }}
+                                />
+                              ) : (
+                                <FaChevronCircleDown
+                                  color={brandTertiary}
+                                  size={doubleSpacer}
+                                  style={{ flexShrink: 0 }}
+                                />
+                              )
+                            }
+                          </AccordionItemState>
+                        </FlexContainer>
+                      </AccordionItemButton>
+                    </AccordionItemHeading>
+                    <AccordionItemPanel>
+                      <div style={{ paddingTop: baseSpacer }}>
+                        <ReactMarkdown source={item.answer} />
+                      </div>
+                    </AccordionItemPanel>
+                  </Box>
+                </AccordionItem>
+              ))}
           </Accordion>
-          <br />
-          <Heading as="h3" styledAs="subtitle">
-            Agent FAQs
-          </Heading>
-          <HorizontalRule />
           <Accordion allowZeroExpanded>
-            {agentFaq.items.map((item) => (
-              <AccordionItem key={item.question}>
-                <AccordionItemHeading>
-                  <AccordionItemButton>
-                    <FlexContainer justifyContent="space-between" alignItems="center">
-                      <Heading as="h4" noMargin>
-                        {item.question}
-                      </Heading>
-                      <AccordionItemState>
-                        {({ expanded }) =>
-                          expanded ? (
-                            <FaChevronCircleUp color={lightestGray} size={doubleSpacer} />
-                          ) : (
-                            <FaChevronCircleDown color={lightestGray} size={doubleSpacer} />
-                          )
-                        }
-                      </AccordionItemState>
-                    </FlexContainer>
-                  </AccordionItemButton>
-                </AccordionItemHeading>
-                <AccordionItemPanel>
-                  <div style={{ paddingTop: baseSpacer }}>
-                    <ReactMarkdown source={item.answer} />
-                  </div>
-                </AccordionItemPanel>
-                <HorizontalRule />
-              </AccordionItem>
-            ))}
+            {filter !== 'consumers' &&
+              agentFaq.items.map((item) => (
+                <AccordionItem key={item.question}>
+                  <Box>
+                    <AccordionItemHeading>
+                      <AccordionItemButton>
+                        <FlexContainer
+                          justifyContent="space-between"
+                          alignItems="center"
+                          flexWrap="nowrap"
+                        >
+                          <Heading as="h4" noMargin>
+                            {item.question}
+                          </Heading>
+                          <AccordionItemState>
+                            {({ expanded }) =>
+                              expanded ? (
+                                <FaChevronCircleUp
+                                  color={brandPrimary}
+                                  size={doubleSpacer}
+                                  style={{ flexShrink: 0 }}
+                                />
+                              ) : (
+                                <FaChevronCircleDown
+                                  color={brandTertiary}
+                                  size={doubleSpacer}
+                                  style={{ flexShrink: 0 }}
+                                />
+                              )
+                            }
+                          </AccordionItemState>
+                        </FlexContainer>
+                      </AccordionItemButton>
+                    </AccordionItemHeading>
+                    <AccordionItemPanel>
+                      <div style={{ paddingTop: baseSpacer }}>
+                        <ReactMarkdown source={item.answer} />
+                      </div>
+                    </AccordionItemPanel>
+                  </Box>
+                </AccordionItem>
+              ))}
           </Accordion>
           <FlexContainer justifyContent="center">
             <Button type="link" to="/">
               Back to Home
             </Button>
           </FlexContainer>
-        </Box>
-      </PageContainer>
-    </section>
-  </div>
-);
+        </PageContainer>
+      </section>
+    </div>
+  );
+};
 
 const FAQPage = ({ data }: { data: { markdownRemark: { frontmatter: FAQPageProps } } }) => {
   const { frontmatter } = data.markdownRemark;
 
-  return (
-    <FAQPageTemplate
-      metaTitle={frontmatter.metaTitle}
-      metaDescription={frontmatter.metaDescription}
-      metaKeywords={frontmatter.metaKeywords}
-      heroImage={frontmatter.heroImage}
-      title={frontmatter.title}
-      heroHeading={frontmatter.heroHeading}
-      heroSubheading={frontmatter.heroSubheading}
-      consumerFaq={frontmatter.consumerFaq}
-      agentFaq={frontmatter.agentFaq}
-    />
-  );
+  return <FAQPageTemplate {...frontmatter} />;
 };
 
 export default FAQPage;
